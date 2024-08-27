4cb310aefd002bdad6af92b510c849bf1cacf6ec
Isaac Dennis <admin@hilantum.com>
2024-08-27 14:09:12 +0000

    Class Example

--- Client.java
+++ Client.java
@@ -4,7 +4,7 @@ public class Client {
         
         System.out.println("\nMake: " + car.getMake());
         System.out.println("Model: " + car.getModel());
-        System.out.println("Owner: " t+ car.getOwner());
+        System.out.println("Owner: " + car.getOwner());
         System.out.println("Sports Car?: " + Boolean.toString(car.isSportsCar()));
         
         System.out.println();
