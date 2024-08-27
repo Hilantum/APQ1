dcc4b9296813d5bf2a43df8a4f0b29f299a9fd07
Isaac Dennis <admin@hilantum.com>
2024-08-27 14:12:48 +0000

    Consistency

--- Client.java
+++ Client.java
@@ -2,14 +2,15 @@ public class Client {
     public static void main(String[] args) {
         Car car = new Car("BMW", "X5M60i", "Isaac Dennis", false);
         
-        System.out.println("\nMake: " + car.getMake());
+        System.out.println();
+        System.out.println("Make: " + car.getMake());
         System.out.println("Model: " + car.getModel());
         System.out.println("Owner: " + car.getOwner());
         System.out.println("Sports Car?: " + Boolean.toString(car.isSportsCar()));
         
-        System.out.println();
         car.setSportsBool(true);
         
+        System.out.println();
         System.out.println("Make: " + car.getMake());
         System.out.println("Model: " + car.getModel());
         System.out.println("Owner: " + car.getOwner());
